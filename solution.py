d2a992776383fb55eda71120407f4a8e8bac49ab
Stephen Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2025-11-14 14:56:59 -0800

    Initial commit

---
+++ solution.py
@@ -0,0 +1,7 @@
+
+def pips_solver(
+    board:list[list[bool]],
+    dominoes: list[tuple[int,int]],
+    region_rules: list[str],
+    regions: list[list[int]]
+) -> list[tuple[float,float,float,float]]
